46a1a3c4146505524ed531e7b1a8ebd7eea3e49d
NickolasKemp <kemplent@gmail.com>
2024-07-19 07:33:42 +0300

    feat: cascading deletes

--- models/customer.model.js
+++ models/customer.model.js
@@ -1,13 +1,20 @@
 const { Schema, model } = require("mongoose");
+const Order = require("./order.model");
 
 const CustomerSchema = new Schema({
-	name: { type: String },
+	name: { type: String, unique: true },
 	street: { type: String },
 	city: { type: String },
 	state: { type: String },
 	zip: { type: Number },
 	phone: { type: String },
 	contactPerson: { type: String },
+});
+
+CustomerSchema.pre("findOneAndDelete", async function (next) {
+	const customerId = this.getQuery()["_id"];
+	await Order.deleteMany({ customer: customerId });
+	next();
 });
 
 module.exports = model("Customer", CustomerSchema);
